d14e2583817aa34dcff543397ead75f5c6fedd1f
zeads <ruri.pelinandang@gmail.com>
2026-04-04 07:25:41 +0700

    feat: add user list

--- resources/views/components/⚡users.blade.php
+++ resources/views/components/⚡users.blade.php
@@ -2,18 +2,40 @@
 
 use Livewire\Component;
 use App\Models\User;
+// use function Livewire\Volt\{computed};
 
 new class extends Component
 {
-    // public $users = User::all();
+
     public function userCount()
     {
         return User::count();
     }
+
+    // $users = computed(fn () => User::all());
+    // $userCount = computed(fn () => User::count());
+
+    public function users()
+    {
+        return User::all();
+    }
+
+    public function addUser()
+    {
+        User::factory()->create();
+    }
 };
 ?>
 
-<div>
+<div class="w-1/2 mx-auto">
     <h1 class="text-3xl">Users Page</h1>
     <p>Number of users: {{ $this->userCount() }}</p>
+    <button wire:click="addUser">Add User</button>
+    <hr class="my-5">
+
+    <ul>
+        @foreach ($this->users() as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>
