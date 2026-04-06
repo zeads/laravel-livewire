af0778ce1dee8135e796e571237a5186199bb141
zeads <ruri.pelinandang@gmail.com>
2026-04-06 13:14:31 +0700

    feat: upload avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
+// use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'avatar'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
--- resources/views/components/⚡users.blade.php
+++ resources/views/components/⚡users.blade.php
@@ -3,9 +3,13 @@
 use Livewire\Component;
 use App\Models\User;
 use Livewire\Attributes\Validate;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Hash;
 
 new class extends Component
 {
+    use WithFileUploads;
+
     #[Validate('required|min:3|max:255')]
     public string $name = '';
 
@@ -14,6 +18,10 @@ new class extends Component
 
     #[Validate('required|min:3|max:255')]
     public string $password = '';
+
+    // #[Validate('image|max:1000')]
+    #[Validate('image|mimes:jpg,jpeg,png|max:1024')]
+    public $avatar;
 
     public function userCount()
     {
@@ -27,12 +35,19 @@ new class extends Component
 
     public function addUser()
     {
-        $this->validate();
+        // $this->validate();
+        $validated = $this->validate();
         // User::factory()->create();
+
+        if($this->avatar) {
+            $validated['avatar'] = $this->avatar->store('avatars', 'public');
+            // $validated['avatar'] = $this->avatar->store(path: 'avatar', name: 'avatar');
+        }
         User::create([
             'name' => $this->name,
             'email' => $this->email,
             'password' => Hash::make($this->password),
+            'avatar' => $validated['avatar'],
         ]);
 
         // $this->reset(['name', 'email', 'password']);
@@ -91,6 +106,40 @@ new class extends Component
                         <p class="mt-2.5 text-xs text-red-600">{{ $message }}</p>
                     @enderror
                 </div>
+
+                <div class="col-span-full">
+                    <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
+                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div class="text-center">
+                        <svg viewBox="0 0 24 24" fill="currentColor" data-slot="icon" aria-hidden="true" class="mx-auto size-12 text-gray-300">
+                            <path d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" fill-rule="evenodd" />
+                        </svg>
+                        <div class="mt-4 flex text-sm/6 text-gray-600">
+                            <label for="avatar" class="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500">
+                            <span>Upload a file</span>
+                            <input wire:model="avatar" id="avatar" type="file" name="avatar" class="sr-only" accept="image/png, image/jpg, image/jpeg" />
+                            </label>
+                            <p class="pl-1">or drag and drop</p>
+                        </div>
+                        <p class="text-xs/5 text-gray-600">PNG, JPG up to 5MB</p>
+                        </div>
+                    </div>
+                    @error('avatar')
+                        <p class="mt-2.5 text-xs text-red-600">{{ $message }}</p>
+                    @enderror
+
+                    {{-- Tambahkan ini sementara untuk debug --}}
+                    {{-- @if($errors->has('avatar.*'))
+                        <p class="text-xs text-red-600">Terjadi kesalahan pada detail file.</p>
+                    @endif
+                     --}}
+                </div>
+
+                @if($avatar)
+                    <p>Preview</p>
+                    <img src="{{ $avatar->temporaryUrl() }}" alt="Preview" class="w-32 h-32 object-cover">
+                @endif
+
 
                 <div>
                     <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create new user</button>
